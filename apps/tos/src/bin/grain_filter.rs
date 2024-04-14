75f921438d1f16c1ed88f9e0d8c4454deedb9cc5
Baptiste Esteban <baptiste.esteban.27@gmail.com>
2024-04-14 14:17:25 +0200

    CLI grain filter

--- apps/tos/src/bin/grain_filter.rs
+++ apps/tos/src/bin/grain_filter.rs
@@ -6,15 +6,28 @@ use olympus::{
 };
 use olympus_tos::{add_median_border, max_interpolation};
 
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    #[arg(name = "input", help = "The input image path")]
+    input_filename: String,
+    #[arg(name = "minimum area", help = "The minimum size of a shape")]
+    min_area: usize,
+    #[arg(name = "output", help = "The output image path")]
+    output_filename: String,
+}
+
 fn main() {
+    let args = Args::parse();
     let mut img = Image2d::<u8>::default();
-    imread(&mut img, "/home/baptou/Pictures/condat/gray/IM106.tif").unwrap();
+    imread(&mut img, &args.input_filename).unwrap();
     let bordered = add_median_border(&img);
     let interpolated = max_interpolation(&bordered);
     let p0 = Point2d::new(0, 0);
     let t = tos(&interpolated, &p0);
     let area = t.accumulate_on_points(CountAccumulator::new());
-    let filtered = t.direct_filter(|n| *area.get(n as usize).unwrap() > 100);
+    let filtered = t.direct_filter(|n| *area.get(n as usize).unwrap() >= args.min_area);
     let rec = filtered.reconstruct();
-    imsave(&rec, "out.png").unwrap();
+    imsave(&rec, &args.output_filename).unwrap();
 }
