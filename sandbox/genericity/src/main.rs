01f4be58a185917ca9c8bb08090e722528590193
Baptiste Esteban <baptiste.esteban@epita.fr>
2025-10-23 19:17:58 +0200

    Image2d watershed binary

--- sandbox/genericity/src/main.rs
+++ sandbox/genericity/src/main.rs
@@ -1,30 +1,29 @@
-//use clap::Parser;
+use clap::Parser;
 use olympus::{
-    cond,
     drawing::label2rgb,
     io::{imread, imsave},
-    labeling::local_minima,
-    morpho::gradient,
-    where_image, Image2d, Mask2d, C4,
+    morpho::{direct_filter, gradient, mintree, reconstruct, watershed_partition},
+    Image2d, Mask2d, C4,
 };
-/*
+
 #[derive(Parser)]
 struct Args {
     input_filename: String,
+    lambda: usize,
     output_filename: String,
-}*/
+}
 
 fn main() {
-    //let args = Args::parse();
+    let args = Args::parse();
 
     let mut img = Image2d::<u8>::default();
-    imread("/home/baptou/Pictures/amazigh.pgm", &mut img).unwrap();
+    imread(args.input_filename.as_str(), &mut img).unwrap();
     let grad = gradient(&img, &Mask2d::cross(3, 3).unwrap());
-    imsave("grad.png", &grad).unwrap();
-    let minima = local_minima(&grad, &C4);
-    let c = cond(&minima, |_, v| *v > 0);
-    let w = where_image(&c, 255u8, 0u8);
-    imsave("minima.png", &w).unwrap();
-    let labeled_colored = label2rgb(&minima);
-    imsave("label_colored.png", &labeled_colored).unwrap();
+    let t = mintree(&grad, &C4);
+    let area = t.compute_area();
+    let tf = direct_filter(&t, |n| area[n] >= args.lambda);
+    let grad_f = reconstruct(&tf);
+    let ws = watershed_partition(&grad_f, &C4);
+    let out = label2rgb(&ws);
+    imsave(args.output_filename.as_str(), &out).unwrap();
 }
