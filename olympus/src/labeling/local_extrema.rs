15b2792c7c9248da3590e3dffc0bf0978520dcef
Baptiste Esteban <baptiste.esteban@epita.fr>
2025-10-23 16:24:50 +0200

    Oupsi (solve bug about ordering)

--- olympus/src/labeling/local_extrema.rs
+++ olympus/src/labeling/local_extrema.rs
@@ -28,10 +28,10 @@ where
             }
 
             match comp(&img[p], &img[n]) {
-                Ordering::Greater => {
+                Ordering::Less => {
                     maybe_an_extrema = false;
                 }
-                Ordering::Less => {
+                Ordering::Greater => {
                     let rn = uf.find(&n);
                     res[rn] = 0;
                 }
