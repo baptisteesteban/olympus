cf97f1523a30b8f46b8e2ea6e0a7e9a5c79cb2bb
Baptiste Esteban <baptiste.esteban@epita.fr>
2025-10-24 11:33:42 +0200

    Generic connected component labeling

--- olympus/src/labeling/connected_components.rs
+++ olympus/src/labeling/connected_components.rs
@@ -1,19 +1,23 @@
-use crate::{Domain, Image, Image2d, ImageMut, Point2d, UnionFind, Window};
+use crate::{Domain, ImageMut, SizedDomain, UnionFind, Window};
 
-pub fn connected_components<V, W>(img: &Image2d<V>, nbh: &W) -> Image2d<u16>
+pub fn connected_components<I, W>(img: &I, nbh: &W) -> I::ChangeValue<u16>
 where
-    V: Eq,
-    W: Window<Point = Point2d>,
+    I: ImageMut,
+    I::Value: Eq + Copy,
+    I::Domain: SizedDomain,
+    <I::Domain as Domain>::Point: Copy + Eq + PartialOrd,
+    W: Window<Point = <I::Domain as Domain>::Point>,
 {
     // Constants
     const UNSEEN: u16 = u16::MAX;
 
     // Data structures
     let mut res = img.imchvalue_with_value::<u16>(UNSEEN);
-    let mut uf = UnionFind::new(unsafe { img.imchvalue_uninitialized::<Point2d>() });
+    let mut uf =
+        UnionFind::new(unsafe { img.imchvalue_uninitialized::<<I::Domain as Domain>::Point>() });
 
     // Connected components
-    for p in *img.domain() {
+    for p in img.domain().clone() {
         uf.make_set(&p);
         res[p] = 1;
         for n in nbh.apply(&p) {
@@ -33,7 +37,7 @@ where
 
     // Labeling
     let mut nlabel = 0;
-    for p in *img.domain() {
+    for p in img.domain().clone() {
         let r = uf.find(&p);
         if r == p {
             nlabel += 1;
